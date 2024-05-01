201c345e65ce93a633992fa4cc9c283281210f7f
mshahzeb95 <shahzebbhatti95@gmail.com>
2024-05-01 16:26:45 +0500

    building ethos phases added

--- src/app/building-ethos/building-ethos.component.ts
+++ src/app/building-ethos/building-ethos.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-building-ethos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './building-ethos.component.html',
-  styleUrl: './building-ethos.component.scss'
+  styleUrl: './building-ethos.component.scss',
 })
 export class BuildingEthosComponent {
+  dynamicHeading: string = 'Building Ethos';
+  dynamicDescription: string =
+    'We’re transparent about our process. Read about our product road map';
 
+  roadmapPhases: { title: string; description: string }[] = [
+    {
+      title: 'Mobilisation',
+      description: 'Lorem ipsum dolor sit amet consectetur.',
+    },
+    {
+      title: 'Phase 1',
+      description: 'Lorem ipsum dolor sit amet consectetur.',
+    },
+    {
+      title: 'Phase 2',
+      description: 'Lorem ipsum dolor sit amet consectetur.',
+    },
+    { title: 'Launch', description: 'Lorem ipsum dolor sit amet consectetur.' },
+  ];
 }
